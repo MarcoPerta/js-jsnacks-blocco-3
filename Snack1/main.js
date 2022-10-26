65421f61e76bcb2b4509f156e0007fb0b30af07a
MarcoPerta <perta.marco@gmail.com>
2022-10-26 14:33:39 +0200

    snack1 messa in tavola passaggi

---
+++ Snack1/main.js
@@ -0,0 +1,12 @@
+// *Snack1*
+// Crea un array vuoto e chiedi all’utente un numero da inserire nell’array.
+// Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.
+
+// -crea un array vuoto
+// -inserisci un prompt per inserire numero nell array
+// -inserisci il dato del prompt nell array
+// -crea ciclo while con i<50
+// -metti prime tre righe nel while
+// -crea variabile somma fuori dal while e ReadableStreamDefaultController, somma tutti i valori inseriti dall'utente e messi nell'array
+
+
