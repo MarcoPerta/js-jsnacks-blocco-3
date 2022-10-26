ba82a62f0b8175fccfcbfa7310079cafac0fa563
MarcoPerta <perta.marco@gmail.com>
2022-10-26 16:31:42 +0200

    snack 4 in corso

---
+++ Snack4/main.js
@@ -0,0 +1,34 @@
+// *Snack4 (Bonus)*
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+// Interrompi il ciclo appena il nome è stato trovato.
+
+// crea array con nomi
+// prompt per chiedere nome e se può patecipare alla festa
+// creare variabile soldatino true per le due opzioni se si verifica true se no false
+// crea if dentro while con due opzioni
+// console log se puo o non può
+
+let invited = ['marco','paolo','luca','gianni'];
+console.log(invited);
+
+let nomeDaTrovare = false;
+
+let i=0;
+while(i<invited.length){
+
+    let ask = parseInt(prompt('inserisci nome'));
+    console.log(ask);
+
+    if(invited[i] === ask){
+        nomeDaTrovare = true;
+    }
+
+    i++
+}
+
+if(nomeDaTrovare==true){
+    console.log('può accedere');
+}
+else{
+    console.log('NON SEI INVITATO');
+}
