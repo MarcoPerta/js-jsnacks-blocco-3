1bb9318ac044ff046b4e5897e6152fc5eeeac055
MarcoPerta <perta.marco@gmail.com>
2022-10-26 15:32:14 +0200

    snack3 in corso

---
+++ Snack3/main.js
@@ -0,0 +1,24 @@
+// *Snack3* (Bonus)
+// Fai inserire un numero, che chiameremo N, all’utente.
+// Genera N array,
+// ognuno formato da 10 numeri casuali da 1 a 100.
+// Ogni volta che ne crei uno, stampalo.
+
+// propmpt per inserire numero N dall'uetnte
+// creare whlie con i<N
+// genera N array
+// N array deve avere 10 numeri casuali da 10 a 100
+
+let n = parseInt(prompt('inserisci un numero'));
+console.log(n)
+
+i=0
+while(i<n){
+  let array = []
+//   console.log(array)
+   while(array.lenght<10){
+    array.push(Math.round(Math.random()*100)+1)
+   }
+   console.log(array)
+  i++
+}
